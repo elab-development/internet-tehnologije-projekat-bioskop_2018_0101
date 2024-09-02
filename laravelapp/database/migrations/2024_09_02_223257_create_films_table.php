3d4e662232d6110227a5f59ab0117a8e37bd9345
andrija999 <andrija99@ptt.rs>
2024-09-03 00:37:48 +0200

    migracije za film

--- laravelapp/database/migrations/2024_09_02_223257_create_films_table.php
+++ laravelapp/database/migrations/2024_09_02_223257_create_films_table.php
@@ -15,6 +15,17 @@ return new class extends Migration
     {
         Schema::create('films', function (Blueprint $table) {
             $table->id();
+            $table->string('naziv');
+            $table->string('zanr');
+            $table->integer('trajanje');
+            $table->text('opis')->nullable();
+            $table->string('reziser')->nullable();
+            $table->text('glumci')->nullable();
+            $table->integer('godina_izdanja')->nullable();
+            $table->string('jezik')->nullable();
+            $table->decimal('ocena', 3, 2)->nullable();
+            $table->string('poster_url')->nullable();
+            $table->string('trailer_url')->nullable();
             $table->timestamps();
         });
     }
